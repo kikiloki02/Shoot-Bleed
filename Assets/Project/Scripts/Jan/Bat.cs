b2b83848c5a4634503258cad72080ca9a449cf5d
Jan <jan.gonzalez@enti.cat>
2021-11-14 02:12:35 +0100

    Finished PJ & Bat Particles + Bat Attack
    The Player and the Bat have the definitive particles.
    The Bat now has 3 different attacks with 3 different particle patterns to analyse:
    - Rush forward (44%)
    - Feint (28%)
    - Chain (28%)

--- Assets/Project/Scripts/Jan/Bat.cs
+++ Assets/Project/Scripts/Jan/Bat.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Bat : Enemy
 {
+    public ParticleSystem _chargingParticlesBasic;
+    public ParticleSystem _chargingParticlesForthAndBack;
+    public ParticleSystem _chargingParticlesChain;
+    public ParticleSystem _attackParticles;
+
+    public AudioManager _audioManager;
+
 // ------ START / UPDATE / FIXEDUPDATE: ------
 
     private void Update()
@@ -19,12 +26,15 @@ public class Bat : Enemy
         {
             Debug.Log("Bat->Dead");
 
+            _audioManager.PlaySFX(3); // Die
+
             _player.GetComponent<Player_Movement_Script>().Heal(5);
 
             Destroy(this.gameObject);
         }
 
         if (Input.GetKeyDown(KeyCode.Y)) { GetHit(1); }
+        if (Input.GetKeyDown(KeyCode.T)) { _audioManager.PlaySFX(0); } // Should be: AudioManager.instance.PlaySFX(value);
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -37,6 +47,10 @@ public class Bat : Enemy
     void Attack1()
     {
         Debug.Log("Bat->Attack1");
+
+        _attackParticles.Play();
+
+        _audioManager.PlaySFX(1); // Attack
 
         _rigidBody.AddForce(_movementSpeed * _chargeDirection * _chargeDistance);
 
@@ -59,9 +73,80 @@ public class Bat : Enemy
 
 // ------ COROUTINES: ------
 
+    IEnumerator Attack2()
+    {
+        Debug.Log("Bat->Attack2");
+
+        _attackParticles.Play();
+
+        _audioManager.PlaySFX(1); // Attack
+
+        Vector3 _storedPosition = this.transform.position;
+
+        _rigidBody.AddForce(_movementSpeed * _chargeDirection * _chargeDistance);
+
+        yield return new WaitForSeconds(0.5f);
+
+        _chargeDirection = _storedPosition - this.transform.position;
+        _chargeDirection.Normalize();
+
+        _attackParticles.Play();
+
+        _rigidBody.AddForce(_movementSpeed * _chargeDirection * _chargeDistance);
+
+        StartCoroutine(AttackCooldown(_cooldownTime));
+    }
+
+    IEnumerator Attack3()
+    {
+        Debug.Log("Bat->Attack3");
+
+        _chargeDirection = _player.GetComponent<Transform>().position - this.transform.position;
+        _chargeDirection.Normalize();
+
+        _attackParticles.Play();
+
+        _audioManager.PlaySFX(1); // Attack
+
+        _rigidBody.AddForce(_movementSpeed * _chargeDirection * _chargeDistance);
+
+        yield return new WaitForSeconds(0.5f);
+
+        _chargeDirection = _player.GetComponent<Transform>().position - this.transform.position;
+        _chargeDirection.Normalize();
+
+        _attackParticles.Play();
+
+        _rigidBody.AddForce(_movementSpeed * _chargeDirection * _chargeDistance);
+
+        StartCoroutine(AttackCooldown(_cooldownTime));
+    }
+
     IEnumerator Charging(float seconds)
     {
         Debug.Log("Bat->Charging");
+
+        int _randomNumber = Random.Range(0, 7); // min included, max excluded
+
+        switch (_randomNumber)
+        {
+            case 0:
+            case 1:
+            case 2:
+                _audioManager.PlaySFX(2); // Charge
+                _chargingParticlesBasic.Play();
+                break;
+            case 3:
+            case 4:
+                _audioManager.PlaySFX(2); // Charge
+                _chargingParticlesForthAndBack.Play();
+                break;
+            case 5:
+            case 6:
+                _audioManager.PlaySFX(2); // Charge
+                _chargingParticlesChain.Play();
+                break;
+        }
 
         _canAttack = false;
 
@@ -76,7 +161,25 @@ public class Bat : Enemy
 
         _spriteRenderer.color = new Color(255, 255, 255);
 
-        Attack1();
+        switch (_randomNumber)
+        {
+            case 0:
+            case 1:
+            case 2:
+                _chargingParticlesBasic.Stop();
+                Attack1();
+                break;
+            case 3:
+            case 4:
+                _chargingParticlesForthAndBack.Stop();
+                StartCoroutine(Attack2());
+                break;
+            case 5:
+            case 6:
+                _chargingParticlesChain.Stop();
+                StartCoroutine(Attack3());
+                break;
+        }
     }
 
     IEnumerator AttackCooldown(float seconds)
@@ -96,6 +199,8 @@ public class Bat : Enemy
 
     IEnumerator GetHitEffect()
     {
+        _audioManager.PlaySFX(3); // Hit
+
         _spriteRenderer.color = new Color(0, 255, 0);
         _spriteRenderer2.color = new Color(0, 255, 0);
         _spriteRenderer3.color = new Color(0, 255, 0);
