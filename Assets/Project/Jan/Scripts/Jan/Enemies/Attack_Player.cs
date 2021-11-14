11705c9fbc6771fef71da878a79bd84eed054572
Isaac <isaac.medina@enti.cat>
2021-11-14 22:04:11 +0100

    Merge Conflicts Solved

---
+++ Assets/Project/Jan/Scripts/Jan/Enemies/Attack_Player.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_Player : MonoBehaviour
+{
+    public GameObject _enemy;
+    public GameObject _player;
+
+    private bool _canDealDamage = true;
+
+    // ------ START / UPDATE / FIXEDUPDATE: ------
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") { _player.GetComponent<PlayerLifeManagement>().GetDamage(_enemy.GetComponent<Enemy>()._attackValue); }
+    }
+
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player" && _canDealDamage) { StartCoroutine(CallGetDamage(_enemy.GetComponent<Enemy>()._attackValue)); }
+    //}
+
+    // ------ METHODS: ------
+
+    /*void CallGetDamage(int damage)
+    {
+        Debug.Log("Player was Hit");
+
+        _player.GetComponent<PlayerLifeManagement>().GetDamage(damage);
+    }*/
+
+    //IEnumerator CallGetDamage(int damage)
+    //{
+    //    Debug.Log("Player was Hit");
+
+    //    _canDealDamage = false;
+
+    //    _player.GetComponent<Player_Controller>().GetHit(damage);
+
+    //    // float seconds = _player.GetComponent<Player_Controller>()._invincibilityTimeBetweenHitsInSeconds;
+    //    yield return new WaitForSeconds(1f); // Wait
+
+    //    _canDealDamage = true;
+    //}
+}
---
+++ Assets/Project/Jan/Scripts/Jan/Enemies/Enemy.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+// ------ PUBLIC: ------
+
+    public int _attackValue;
+    public int _movementSpeed;
+    public int _chargeDistance;
+    public float _chargeTime;
+    public float _cooldownTime;
+
+    public Rigidbody2D _rigidBody;
+    public Collider2D _attackDetectionZone;
+    public SpriteRenderer _spriteRenderer;
+    public SpriteRenderer _spriteRenderer2;
+    public SpriteRenderer _spriteRenderer3;
+    public GameObject _player;
+
+// ------ PROTECTED: ------
+
+    protected float _hitEffectDuration = 0.15f;
+    protected bool _gotHit;
+    protected Vector2 _chargeDirection;
+    protected Color _sprite1Color;
+    protected Color _sprite2Color;
+    protected Color _sprite3Color;
+    protected bool _canAttack = true;
+
+    // ------ METHODS: ------
+
+    public virtual void Die() {}
+
+    public virtual void HealPlayer() {}
+
+    public virtual void GetHit() {}
+
+    public virtual bool AmIDead() { return true; }
+
+    public virtual IEnumerator GetHitEffect() { yield return new WaitForSeconds(0f); }
+}
