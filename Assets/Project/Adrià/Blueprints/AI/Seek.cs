5bfba2054f42ebd0102ab98b9500a8ed37335fd9
Adrià <adria.campos@enti.cat>
2021-12-10 20:34:05 +0100

    Almost Finished Pathfinding

--- Assets/Project/Adrià/Blueprints/AI/Seek.cs
+++ Assets/Project/Adrià/Blueprints/AI/Seek.cs
@@ -4,32 +4,45 @@ using UnityEngine;
 
 public class Seek : MonoBehaviour
 {
-    
+    private GameObject player;
     private Transform target;
     private Rigidbody2D rb;
     public float velocity;
     public float avoidVelocity;
-    public Vector2 targetDirection = Vector2.zero;
+
+    public Vector3 targetDirection = Vector3.zero;
+    public Vector3 RightPerpendicularTargetDirection = Vector3.zero;
+    public Vector3 LeftPerpendicularTargetDirection = Vector3.zero;
 
     float maxSeeAhead = 1.0f;
     float xSize, ySize;
 
     Vector3 topLeft, topRight,bottomLeft, bottomRight, center, bottomMid, topMid;
 
-    public const float avoidingPercentage = 0.8f;
+    public const float avoidingPercentage = 0.9f;
     public float followingPercentage = 1 - avoidingPercentage;
 
 
     void Start()
     {
-        target = FindObjectOfType<Player_Controller>().gameObject.transform;
+        player = FindObjectOfType<Player_Controller>().gameObject;
         rb = GetComponent<Rigidbody2D>();
 
         SetInitialBoundingBoxInfo();
     }
+    private void Update()
+    {
+        target = player.transform;
+        targetDirection = player.transform.position - this.transform.position;
+
+        RightPerpendicularTargetDirection = new Vector3(targetDirection.y, -targetDirection.x, targetDirection.z);
+        LeftPerpendicularTargetDirection = new Vector3(-targetDirection.y, targetDirection.x, targetDirection.z);
+
+    }
 
     void FixedUpdate()
     {
+
         CreateVirtualBoundingBox();
         CheckForCollisionDetected();
     }
@@ -37,85 +50,94 @@ public class Seek : MonoBehaviour
     private void SetInitialBoundingBoxInfo()
     {
         /* Calculate some data that will be used to create the bounding box around the agent */
-        float currentZRotation = transform.eulerAngles.z;
-        transform.rotation = Quaternion.Euler(Vector3.zero);
+        //float currentZRotation = transform.eulerAngles.z;
+        //transform.rotation = Quaternion.Euler(Vector3.zero);
         xSize = GetComponent<SpriteRenderer>().bounds.size.x;
         ySize = GetComponent<SpriteRenderer>().bounds.size.y;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, currentZRotation));
+        //transform.rotation = Quaternion.Euler(new Vector3(0, 0, currentZRotation));
     }
 
     private void CreateVirtualBoundingBox()
     {
-        center = transform.position;
+        center = this.transform.position;
+        topMid = transform.position + targetDirection.normalized * maxSeeAhead;
+        topRight = transform.position + (targetDirection.normalized + RightPerpendicularTargetDirection.normalized) * (maxSeeAhead); 
+        topLeft = transform.position + (targetDirection.normalized + LeftPerpendicularTargetDirection.normalized) * (maxSeeAhead); 
+        
+        //if (targetDirection.y > 0 && targetDirection.y > targetDirection.x)
+        //{
 
-        if (targetDirection.y > 0 && targetDirection.y > targetDirection.x)
-        {
+        ////UP
+        //    /* Create the bounding box around the sprite for collision detection */
+        //    //bottomRight = transform.position + (transform.right * (xSize / 2)) + (-transform.up * (ySize / 2));
+        //    //bottomLeft = transform.position + (-transform.right * (xSize / 2)) + (-transform.up * (ySize / 2));
 
-        //UP
-            /* Create the bounding box around the sprite for collision detection */
-            //bottomRight = transform.position + (transform.right * (xSize / 2)) + (-transform.up * (ySize / 2));
-            //bottomLeft = transform.position + (-transform.right * (xSize / 2)) + (-transform.up * (ySize / 2));
+        //    topRight = transform.position + ((transform.right * (xSize/2)) + (transform.up * maxSeeAhead));
+        //    topLeft = transform.position + (-transform.right * (xSize/2)) + (transform.up * maxSeeAhead);
 
-            topRight = transform.position + ((transform.right * (xSize/2)) + (transform.up * maxSeeAhead));
-            topLeft = transform.position + (-transform.right * (xSize/2)) + (transform.up * maxSeeAhead);
-
-            bottomMid = transform.position + (transform.right * 0) + (-transform.up * (ySize/2));
-            topMid = transform.position + ((transform.right * 0) + (transform.up * (maxSeeAhead )));
+        //    bottomMid = transform.position + (transform.right * 0) + (-transform.up * (ySize/2));
+        //    topMid = transform.position + ((transform.right * 0) + (transform.up * (maxSeeAhead )));
 
 
-        }
-        //LEFT
-        else if (targetDirection.x < 0 && (-(targetDirection.x) > targetDirection.y))
-        {
-            //bottomRight = transform.position + (-transform.up * (xSize / 2)) + (transform.right * (ySize / 2));
-            //bottomLeft = transform.position + (transform.up * (xSize / 2)) + (transform.right * (ySize / 2));
+        //}
+        ////LEFT
+        //else if (targetDirection.x < 0 && (-(targetDirection.x) > targetDirection.y))
+        //{
+        //    //bottomRight = transform.position + (-transform.up * (xSize / 2)) + (transform.right * (ySize / 2));
+        //    //bottomLeft = transform.position + (transform.up * (xSize / 2)) + (transform.right * (ySize / 2));
 
-            topRight = transform.position + ((-transform.up * (xSize / 2)) + (-transform.right * maxSeeAhead));
-            topLeft = transform.position + (transform.up * (xSize / 2)) + (-transform.right * maxSeeAhead);
+        //    topRight = transform.position + ((-transform.up * (xSize / 2)) + (-transform.right * maxSeeAhead));
+        //    topLeft = transform.position + (transform.up * (xSize / 2)) + (-transform.right * maxSeeAhead);
 
-            bottomMid = transform.position + (-transform.right * 0) + (-transform.right * (ySize / 2));
-            topMid = transform.position + ((transform.right * 0) + (-transform.right * (maxSeeAhead)));
+        //    bottomMid = transform.position + (-transform.right * 0) + (-transform.right * (ySize / 2));
+        //    topMid = transform.position + ((transform.right * 0) + (-transform.right * (maxSeeAhead)));
 
-        }
-        //DOWN
-        else if (targetDirection.y < 0 && (-(targetDirection.x) > targetDirection.y))
-        {  
-            //bottomRight = transform.position + (-transform.right * (xSize / 2)) + (transform.up * (ySize / 2));
-            //bottomLeft = transform.position + (transform.right * (xSize / 2)) + (transform.up * (ySize / 2));
+        //}
+        ////DOWN
+        //else if (targetDirection.y < 0 && (-(targetDirection.x) > targetDirection.y))
+        //{  
+        //    //bottomRight = transform.position + (-transform.right * (xSize / 2)) + (transform.up * (ySize / 2));
+        //    //bottomLeft = transform.position + (transform.right * (xSize / 2)) + (transform.up * (ySize / 2));
 
-            topRight = transform.position + ((-transform.right * (xSize / 2)) + (-transform.up * maxSeeAhead));
-            topLeft = transform.position + (transform.right * (xSize / 2)) + (-transform.up * maxSeeAhead);
+        //    topRight = transform.position + ((-transform.right * (xSize / 2)) + (-transform.up * maxSeeAhead));
+        //    topLeft = transform.position + (transform.right * (xSize / 2)) + (-transform.up * maxSeeAhead);
 
-            bottomMid = transform.position + (transform.right * 0) + (transform.up * (ySize / 2));
-            topMid = transform.position + ((transform.right * 0) + (-transform.up * (maxSeeAhead)));
+        //    bottomMid = transform.position + (transform.right * 0) + (transform.up * (ySize / 2));
+        //    topMid = transform.position + ((transform.right * 0) + (-transform.up * (maxSeeAhead)));
 
-        }
-        //RIGHT
-        else if (targetDirection.x > 0 && targetDirection.x > targetDirection.y)
-        {
-            //bottomRight = transform.position + (transform.up * (xSize / 2)) + (-transform.right * (ySize / 2));
-            //bottomLeft = transform.position + (-transform.up * (xSize / 2)) + (-transform.right * (ySize / 2));
+        //}
+        ////RIGHT
+        //else if (targetDirection.x > 0 && targetDirection.x > targetDirection.y)
+        //{
+        //    //bottomRight = transform.position + (transform.up * (xSize / 2)) + (-transform.right * (ySize / 2));
+        //    //bottomLeft = transform.position + (-transform.up * (xSize / 2)) + (-transform.right * (ySize / 2));
 
-            topRight = transform.position + ((transform.up * (xSize / 2)) + (transform.right * maxSeeAhead));
-            topLeft = transform.position + (-transform.up * (xSize / 2)) + (transform.right * maxSeeAhead);
+        //    topRight = transform.position + ((transform.up * (xSize / 2)) + (transform.right * maxSeeAhead));
+        //    topLeft = transform.position + (-transform.up * (xSize / 2)) + (transform.right * maxSeeAhead);
 
-            bottomMid = transform.position + (transform.right * 0) + (transform.right * (ySize / 2));
-            topMid = transform.position + ((transform.right * 0) + (transform.right * (maxSeeAhead)));
-        }
-        topRight = Quaternion.Euler(0, 0, -45f) * (this.transform.position + this.transform.up);
-        topLeft = Quaternion.Euler(0, 0, 45f) * (this.transform.position + this.transform.up);
-        topMid = Quaternion.Euler(0, 0, 0f) * (this.transform.position + this.transform.up);
+        //    bottomMid = transform.position + (transform.right * 0) + (transform.right * (ySize / 2));
+        //    topMid = transform.position + ((transform.right * 0) + (transform.right * (maxSeeAhead)));
+        //}
+        
+        //float rotZ = Mathf.Atan2(targetDirection.normalized.y, targetDirection.normalized.x) * Mathf.Rad2Deg;
+        
+        //topRight = Quaternion.Euler(0, 0, (rotZ+90)) * (target.transform.position + target.transform.up);
+        //topLeft = Quaternion.Euler(0, 0, (rotZ-90)) * (target.transform.position + target.transform.up);
+        //topMid = Quaternion.Euler(0, 0, 0f) * (target.transform.position + target.transform.up);
+
         //Draw Raycast Lines
         //Debug.DrawRay(bottomRight, (bottomLeft - bottomRight), Color.green);
-        Debug.DrawRay(topRight, (topLeft - topRight), Color.green);
+        //Debug.DrawRay(topRight, (topLeft - topRight), Color.green);
 
         //Debug.DrawRay(bottomRight, (topRight - bottomRight), Color.green);
         //Debug.DrawRay(bottomLeft, (topLeft - bottomLeft), Color.green);
 
-        Debug.DrawRay(center, (topRight - center), Color.red);
-        Debug.DrawRay(center, (topLeft - center), Color.red);
-
+        //Debug.DrawRay(center, (topRight - center), Color.red);
+        //Debug.DrawRay(center, (topLeft - center), Color.red);
+        
         Debug.DrawRay(center, (topMid - center), Color.green);
+        Debug.DrawRay(center, (topRight - center), Color.red);
+        Debug.DrawRay(center, (topLeft - center), Color.yellow);
         
 
     }
@@ -128,9 +150,9 @@ public class Seek : MonoBehaviour
 
         /* 2 raycasts are used for this, one points from the bottom left corner to the top left corner of the agent and
         the other from the bottom right to the top right */
-        hit2D[0] = Physics2D.Raycast(bottomLeft, topLeft - bottomLeft, maxSeeAhead,mask);
-        hit2D[1] = Physics2D.Raycast(bottomRight, topRight - bottomRight, maxSeeAhead,mask);
-        hit2D[2] = Physics2D.Raycast(bottomMid, topMid - bottomMid, maxSeeAhead,mask);
+        hit2D[0] = Physics2D.Raycast(center, topLeft - center, (maxSeeAhead),mask);
+        hit2D[1] = Physics2D.Raycast(center, topRight - center, (maxSeeAhead),mask);
+        hit2D[2] = Physics2D.Raycast(center, topMid - center, maxSeeAhead,mask);
       
 
 
@@ -168,9 +190,10 @@ public class Seek : MonoBehaviour
         }
         else if (hit2D[2])
         {
-            dirOfMovementToAvoidObstacle = bottomMid - hit2D[2].collider.transform.position;
+            dirOfMovementToAvoidObstacle = center - hit2D[2].collider.transform.position;
             dirOfMovementToAvoidObstacle *= Vector2.Distance(transform.position, hit2D[2].collider.transform.position);
-            rb.AddForce(dirOfMovementToAvoidObstacle * (velocity-33) );
+            rb.AddForce(dirOfMovementToAvoidObstacle * (avoidVelocity * avoidingPercentage));
+            rb.AddForce(targetDirection.normalized * ((velocity+20) * followingPercentage));
 
             Debug.DrawRay(hit2D[2].collider.transform.position, bottomMid - hit2D[2].collider.transform.position, Color.white);
         }
@@ -178,7 +201,6 @@ public class Seek : MonoBehaviour
         /* If no obstacle was detected, then just steer it towards it's current velocity */
         else
         {
-            targetDirection = target.transform.position - this.transform.position;
             rb.AddForce(targetDirection.normalized * velocity);
         }
     }
