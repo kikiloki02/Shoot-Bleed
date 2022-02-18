c87240051e168aad2278fe062415ab10209e01bd
Jan <jan.gonzalez@enti.cat>
2022-02-18 20:27:15 +0100

    Healer now heals the correct amount of health

--- Assets/Project/Isaac/Scripts/HealthSystem.cs
+++ Assets/Project/Isaac/Scripts/HealthSystem.cs
@@ -6,6 +6,9 @@ public class HealthSystem : MonoBehaviour
 {
     public int maxHealth;
     public int currentHealth;
+
+    public int _healPlayer;
+
     public GameObject healer;
 
     public GameObject enemy;
@@ -38,8 +41,10 @@ public class HealthSystem : MonoBehaviour
             {
                 GameObject Healer;
                 //FindObjectOfType<PlayerLifeManagement>().RecoverHealth(enemy.GetComponent<Enemy>()._healPlayer);
+
                 Healer = Instantiate(healer, this.gameObject.transform.position, Quaternion.Euler(0,0,0));
 
+                Healer.GetComponent<Healer>().SetPointsToHeal(_healPlayer);
             }
 
             Destroy(this.gameObject); // <-- AQUÍ
