a66384bf993d8a09b534ed26979dd9d09c199759
Adrià <mangaadri@gmail.com>
2022-04-06 14:01:47 +0200

    Starting With the Shop

--- Assets/Project/Scripts/Shop/ItemOutRun.cs
+++ Assets/Project/Scripts/Shop/ItemOutRun.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ItemOutRun : MonoBehaviour
 {
+    int price;
+
     // Start is called before the first frame update
     void Start()
     {
